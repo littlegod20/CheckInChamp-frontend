a922269e9d8ed520da1bd751b0492ed41a7f2ff1
Misheal384 <eversonmisheal2@gmail.com>
2025-01-30 21:36:53 +0000

    added kudosdashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,23 +12,28 @@ import NotFoundPage from './components/pages/NotFoundPage';
 import Reports from './components/pages/Reports';
 import StandupDetailsPage from './components/pages/StandupDetailsPage';
 import AnswerStandup from './components/pages/AnswerStandup';
+import KudosHistory from './components/pages/KudosHistory';
+import KudosDashboard from './components/pages/KudosDashboard';
+import Leaderboard from './components/pages/Leaderboard';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
-  { path: '/teams/', element: <ManageTeam /> },
-  { path: '/add-team', element: <CreateTeamPage /> },
-  { path: '/remove-team', element: <RemoveTeam /> },
-  { path: '/add-team-member/:teamId', element: <AddMemberPage/> },
-  { path: '/remove-team-member', element: <RemoveMember /> },
-  { path: '/configure-standup-questions/:teamId', element: <ConfigureStandup /> },
-  {path: '/standup-answer/:teamId', element: <AnswerStandup />},
+  { path: '/teams', element: <ManageTeam /> },
   { path: '/create-team', element: <CreateTeamPage /> },
+  { path: '/remove-team', element: <RemoveTeam /> },
   { path: '/teams/:teamId/members', element: <AddMemberPage /> },
+  { path: '/teams/:teamId/members/:memberId/remove', element: <RemoveMember /> },
+  { path: '/configure-standup-questions/:teamId', element: <ConfigureStandup /> },
+  { path: '/standup-answer/:teamId', element: <AnswerStandup /> },
   { path: '/teams/:teamId/members/:memberId/standup', element: <StandupPage /> },
-  { path: '/standup-collection/:id', element: <StandupDetailsPage /> }, // Add this route
+  { path: '/standup-collection/:id', element: <StandupDetailsPage /> },
   { path: '/reports', element: <Reports /> },
+  { path: '/kudos-history', element: <KudosHistory /> },
+  { path: '/kudos-dashboard', element: <KudosDashboard /> },
+  { path: '/leaderboard', element: <Leaderboard /> },
   { path: '*', element: <NotFoundPage /> },
 ]);
+
 
 const Routes: React.FC = () => {
   return <RouterProvider router={router} />;
