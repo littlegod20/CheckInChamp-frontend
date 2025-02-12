5993ce5999ba8ce6b82df93f04c77c5fe4686168
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-02-12 10:35:01 +0000

    fixing tailwind bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/HomePage";
 import ManageTeam from "./pages/TeamPage";
 import MainLayout from "./layout";
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,4 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 };
-
